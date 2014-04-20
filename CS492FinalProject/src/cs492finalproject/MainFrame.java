473bd8dc5d36f91f7848ca31fc698b6141198c7b
JGR <JGR@192.168.0.2>
2014-04-19 23:44:53 -0400

    exported thread

--- CS492FinalProject/src/cs492finalproject/MainFrame.java
+++ CS492FinalProject/src/cs492finalproject/MainFrame.java
@@ -253,47 +253,8 @@ public class MainFrame extends javax.swing.JFrame {
       final int userVal = Integer.parseInt(spinPacketNumber.getValue().toString());
       final int numPackets = (userVal == 0) ? Pcap.LOOP_INFINITE : userVal;
 
-      new Thread() {
-        @Override
-        public void run() {
-          while (tbtnCapture.isSelected()) {
-            txtaLog.append("\nBeginning Packet Capture ["
-                + ((userVal == 0) ? "Infinity" : userVal) + "]:\n\n");
-            int snaplen = 64 * 1024;           // Capture all packets, no trucation  
-            int flags = Pcap.MODE_PROMISCUOUS; // capture all packets  
-            int timeout = 10 * 1000;           // 10 seconds in millis    
-            pcap = Pcap.openLive(
-                alldevs.get(
-                    cboxDevice.getSelectedIndex()).getName(), snaplen, flags, timeout, errbuf);
-
-            if (pcap == null) {
-              txtaLog.append("Error while opening device for capture: " + errbuf.toString() + "\n");
-              return;
-            }
-
-            PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
-              @Override
-              public void nextPacket(final PcapPacket packet, final String user) {
-                txtaLog.append("Received packet at " + new Date(packet.getCaptureHeader().timestampInMillis())
-                    + "\tcaplen=" + packet.getCaptureHeader().caplen()
-                    + "\tlen=" + packet.getCaptureHeader().wirelen()
-                    + "\t" + user + "\n");
-              }
-            };
-
-            pcap.loop(numPackets, jpacketHandler, "IDS System");
-
-            txtaLog.append("\nCapture finished. Closing link to PCAP.\n");
-            tbtnCapture.setSelected(false);
-          }
-          // Clean up after stop requested.
-          try {
-            pcap.close();
-          } catch (Exception e) {
-            txtaLog.append("Link to PCAP is already closed, trying anyway.\n");
-          }
-        }
-      }.start();
+      Thread pcapThread = new Thread(new ThreadPCAP(userVal, numPackets, tbtnCapture, txtaLog, pcap, cboxDevice, alldevs, errbuf));
+      pcapThread.start();
     }//GEN-LAST:event_tbtnCaptureActionPerformed
 
     private void tbtnCaptureStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_tbtnCaptureStateChanged
