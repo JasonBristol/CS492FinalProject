a926da8d9e1e447b809c5fde9105b96e617eb7cb
JGR <JGR@192.168.0.2>
2014-04-22 14:49:57 -0400

    Merge origin/master
    Conflicts:
    	CS492FinalProject/src/cs492finalproject/IDS/PacketCapture.java

--- CS492FinalProject/src/cs492finalproject/IDS/PacketCapture.java
+++ CS492FinalProject/src/cs492finalproject/IDS/PacketCapture.java
@@ -51,6 +51,11 @@ public class PacketCapture implements Runnable, LogInterface {
 
   @Override
   public void run() {
+    // Create Analyzer Thread
+    final PacketHeaderAnalyzer PHA = new PacketHeaderAnalyzer((txtaLog));
+    Thread analyzer = new Thread(PHA);
+    analyzer.start();
+    
     while (isCapturing) { // Don't do anything unless isCapturing is true
       appendLog(txtaLog, "\nBeginning Packet Capture ["
           + ((userVal == 0) ? "Infinity" : userVal) + "]:\n\n");
@@ -65,7 +70,7 @@ public class PacketCapture implements Runnable, LogInterface {
         appendLog(txtaLog, "Error while opening device for capture: " + errbuf.toString() + "\n");
         return;
       }
-
+      
       PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
 
         Tcp tcp = new Tcp();
@@ -78,7 +83,11 @@ public class PacketCapture implements Runnable, LogInterface {
           if (!isCapturing) {
             pcap.breakloop(); // Break the loop and exit
           }
-
+          
+          // Add packet to Analzyer Thread
+          PcapPacket clone = packet;
+          PHA.addPacket(clone);
+          
           if (packet.hasHeader(ipv4)) {
             // If it has an IPv4 header, lets build some strings
             srcIP = org.jnetpcap.packet.format.FormatUtils.ip(packet.getHeader(ipv4).source());
@@ -107,6 +116,8 @@ public class PacketCapture implements Runnable, LogInterface {
       appendLog(txtaLog, "\nCapture finished. Link to PCAP closed.\n");
       isCapturing = false; // Stop processing
       tbtnCapture.setSelected(false); // Reset button
+      // Interrupt Analzyer Thread
+      analyzer.interrupt();
     }
     // Your here if isCapturing is false
     // Clean up and destroy thread.
--- CS492FinalProject/src/cs492finalproject/IDS/PacketHeaderAnalyzer.java
+++ CS492FinalProject/src/cs492finalproject/IDS/PacketHeaderAnalyzer.java
@@ -6,10 +6,53 @@
 
 package cs492finalproject.IDS;
 
+import java.util.LinkedList;
+import javax.swing.JTextArea;
+import org.jnetpcap.packet.PcapPacket;
+import org.jnetpcap.protocol.network.Ip4;
+import org.jnetpcap.protocol.tcpip.Tcp;
+
 /**
  *
  * @author JBristol
  */
-public class PacketHeaderAnalyzer {
-  
+public class PacketHeaderAnalyzer implements Runnable {
+    private LinkedList<PcapPacket> packets;
+    private JTextArea txtArea;
+    
+    public PacketHeaderAnalyzer(JTextArea txtArea) {
+        this.txtArea = txtArea;
+        packets = new LinkedList<PcapPacket>();
+        
+    }
+
+    @Override
+    public void run() {
+        Tcp tcp = new Tcp();
+        while(!Thread.interrupted()){
+            if (!packets.isEmpty()) {
+                PcapPacket currentPacket = packets.removeFirst();
+                if (currentPacket.hasHeader(tcp)) checkTcpFlags(currentPacket);
+            }
+        }
+        appendLog(txtArea, "Terminating Analzyer Thread.");
+    }
+    
+    private void checkTcpFlags(PcapPacket packet){
+        Tcp tcp = new Tcp();
+        Ip4 ipv4 = new Ip4();
+        //WORK IN PROGRESS
+        if(packet.getHeader(tcp).flags_ACK()) {
+            //appendLog(txtArea, org.jnetpcap.packet.format.FormatUtils.ip(packet.getHeader(ipv4).source()) + "is ACK \n");
+        }
+    }
+    
+    public void addPacket(PcapPacket packet){
+        packets.add(packet);
+    }
+    
+    private void appendLog(JTextArea log, String message) {
+    log.append(message);
+    log.setCaretPosition(log.getText().length());
+  }
 }
