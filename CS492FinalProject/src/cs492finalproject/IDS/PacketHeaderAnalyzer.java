106b1105395cf63e0cbb3d48791244eb01bc4183
JGR <JGR@192.168.0.2>
2014-04-23 06:49:29 -0400

    set Scan Detector to output to PHA Panel

--- CS492FinalProject/src/cs492finalproject/IDS/PacketHeaderAnalyzer.java
+++ CS492FinalProject/src/cs492finalproject/IDS/PacketHeaderAnalyzer.java
@@ -77,8 +77,18 @@ public class PacketHeaderAnalyzer implements Runnable, LogInterface {
                   if(packet.getHeader(tcp).flags_FIN()) iFIN++;
               }
           }
-          appendLog(txtArea,"t:" + total + " i:" + incoming + " o:" + outgoing + " c1:" + iSYNnACK + " c2:" + oSYNACK + " c3:" + oRST + " c4:" + oSYNnACK + " c5:" + iSYNACK + " c6:" + oFIN + " c7:" + iFIN + " p1:" + P1 +  " p2:" + P2 + " p3:" + P3 + "\n", Color.black);
-          
+          //appendLog(txtArea,"t:" + total + " i:" + incoming + " o:" + outgoing + " c1:" + iSYNnACK + " c2:" + oSYNACK + " c3:" + oRST + " c4:" + oSYNnACK + " c5:" + iSYNACK + " c6:" + oFIN + " c7:" + iFIN + " p1:" + P1 +  " p2:" + P2 + " p3:" + P3 + "\n", Color.black);
+          //update PHA Panel
+          packetPanes[0].setText(total + "");
+          packetPanes[1].setText(incoming + "");
+          packetPanes[2].setText(iSYNnACK + "");
+          packetPanes[3].setText(iSYNACK + "");
+          packetPanes[4].setText(iFIN + "");
+          packetPanes[5].setText(outgoing + "");
+          packetPanes[6].setText(oSYNnACK + "");
+          packetPanes[7].setText(oSYNACK + "");
+          packetPanes[8].setText(oRST + "");
+          packetPanes[9].setText(oFIN + "");
       } catch (UnknownHostException ex) {
           Logger.getLogger(PacketHeaderAnalyzer.class.getName()).log(Level.SEVERE, null, ex);
       }
